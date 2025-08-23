3b46e95a4b101496347299e328b9cef05c98eb6c
Olar Sa'id <134246135+ol-s-cloud@users.noreply.github.com>
2025-08-23 01:17:40 +0100

    Update 004_broadcast_idx.sql

--- sql/004_broadcast_idx.sql
+++ sql/004_broadcast_idx.sql
@@ -1,1 +1,4 @@
-
+-- Partial index: WHERE status='broadcast' ORDER BY created_at
+CREATE INDEX IF NOT EXISTS idx_requests_broadcast_created_at
+  ON requests (created_at)
+  WHERE status = 'broadcast';
